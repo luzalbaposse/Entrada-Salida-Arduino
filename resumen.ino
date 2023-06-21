826b204c17d097a89b14eaec36900a191dbbe91f
Luz Alba Posse <66437312+luzalbaposse@users.noreply.github.com>
2023-06-21 09:08:58 -0300

    Update resumen.ino

--- resumen.ino
+++ resumen.ino
@@ -1,10 +1,14 @@
 /*
 Utilizado lo aprendido en los puntos anteriores construir un programa que respete el siguiente
 comportamiento:
+
 Los leds van a mostrar contando del 000 al 111 la posici´on del Joystick en sentido vertical.
+
 El movimiento del Joystick en sentido horizontal deber´a mover el Servo en todo su rango.
+
 Si se presiona el botón del Joystick se intercambiar´a el comportamiento de los ejes vertical y
 horizontal.
+
 */
 #include <Servo.h>
 
@@ -18,6 +22,7 @@ const int ledPin2 = 6;
 const int VRy = A0;
 const int VRx = A1;
 const int SW = 2;
+int time_stamp = 0;
 
 void setup() {
   servo1.attach(servoPin);
@@ -29,36 +34,48 @@ void setup() {
   pinMode(VRx, INPUT);
   pinMode(VRy, INPUT);
   pinMode(SW, INPUT_PULLUP);
+
+  // Estado inicial: LEDs verticales, servomotor horizontal
+  int horizontal = 90;  // Posición horizontal media para el servomotor
+  int vertical = 0;    // Valor inicial para los LEDs verticales
+  servo1.write(horizontal);
+  digitalWrite(ledPin0, (vertical & 1));
+  digitalWrite(ledPin1, (vertical & 2));
+  digitalWrite(ledPin2, (vertical & 4));
 }
 
 void loop() {
   int xPosition = analogRead(VRx);
   int yPosition = analogRead(VRy);
-  int SW_state = digitalRead(SW);
-  int horizontal = map(xPosition, 0, 1023, 0, 180);
-  servo1.write(horizontal);
-  int vertical = map(yPosition, 0, 1023, 0, 7);
-  int led0 = vertical & 1;
-  int led1 = vertical & 2;
-  int led2 = vertical & 4;
-  digitalWrite(ledPin0, led0);
-  digitalWrite(ledPin1, led1);
-  digitalWrite(ledPin2, led2);
-  
+  int horizontal, vertical;
+
   if (button) {
-    int aux = horizontal;
-    int aux2 = vertical;
-    horizontal = aux2;
-    vertical = aux;
+    horizontal = map(yPosition, 0, 1023, 0, 180);
+    vertical = map(xPosition, 0, 1023, 0, 7);
+  } else {
+    horizontal = map(xPosition, 0, 1023, 0, 180);
+    vertical = map(yPosition, 0, 1023, 0, 7);
   }
-  
-  delay(100); // Agrega un retraso de 100 milisegundos para evitar que se ejecute demasiado rápido. El objetivo es ayudar a estabilizar la lectura del joystick 
+
+  servo1.write(horizontal);
+
+  digitalWrite(ledPin0, (vertical & 1));
+  digitalWrite(ledPin1, (vertical & 2));
+  digitalWrite(ledPin2, (vertical & 4));
+
+  if (time_stamp > 0) {
+    time_stamp = time_stamp - 1;
+  }
+
+  delay(2);
 }
 
 void click() {
-  if (button == 0) {
-    button = 1;
-  } else {
-    button = 0;
+  if (time_stamp == 0) {
+    if (button == 0) {
+      button = 1;
+    } else {
+      button = 0;
+    }
   }
 }
